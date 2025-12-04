d433f286d908e1bd2f04f63c213f592db0dd2cc4
Iñaki Josep Fernández de V <i.fviana@dti.uhu.es>
2025-12-04 18:42:23 +0000

    Update wildintel tools version and refactor commands

--- utils/install_wildintel_tools.ps1
+++ utils/install_wildintel_tools.ps1
@@ -4,7 +4,7 @@
 # -------------------------------------
 # Configuration
 # -------------------------------------
-$wildintel_tools_version = "v0.0.1"
+$wildintel_tools_version = "v0.1.0"
 $repo_url = "https://github.com/ijfvianauhu/wildintel-tools.git"
 $repo_dir = "wildintel-tools"
 
@@ -19,74 +19,27 @@ Write-Host ""
 # Check dependencies
 # -------------------------------------
 
-function Check-Command($cmd, $name) {
+function Check-Command {
+    param(
+        [string]$cmd,
+        [string]$name
+    )
+
     if (-not (Get-Command $cmd -ErrorAction SilentlyContinue)) {
         Write-Host "✕ $name NO está instalado." -ForegroundColor Red
         return $false
     }
+
     Write-Host "✓ $name encontrado." -ForegroundColor Green
     return $true
 }
 
+
 Write-Host "Checking dependencies..."
 
-$hasGit = Check-Command "git" "Git"
-$hasUV = Check-Command "uv" "uv"
+$hasGit = Check-Command -cmd "git" -name "Git"
+$hasUV = Check-Command -cmd "uv" -name "uv"
+$hasWinget = Check-Command -cmd "winget" -name "winget"
 
 if (-not $hasGit -or -not $hasUV) {
-    Write-Host ""
-    Write-Host "✕ No se pueden continuar porque faltan dependencias requeridas (Git o UV)." -ForegroundColor Red
-    exit 1
-}
-
-Write-Host ""
-Write-Host "✓ Dependencias mínimas presentes. Continuando..."
-Write-Host ""
-
-
-# -------------------------------------
-# Clone repository
-# -------------------------------------
-
-if (Test-Path $repo_dir) {
-    Write-Host "El directorio '$repo_dir' ya existe. Usaré ese." -ForegroundColor Yellow
-} else {
-    Write-Host "Clonando repositorio..."
-    git clone $repo_url
-
-    if ($LASTEXITCODE -ne 0) {
-        Write-Host "✕ Error clonando el repositorio." -ForegroundColor Red
-        exit 1
-    }
-}
-
-Set-Location $repo_dir
-
-Write-Host "Cambiando a la versión $wildintel_tools_version..."
-git checkout $wildintel_tools_version
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "✕ No se pudo cambiar a la versión especificada." -ForegroundColor Red
-    exit 1
-}
-
-Write-Host "✓ Repositorio listo." -ForegroundColor Green
-Write-Host ""
-
-
-# -------------------------------------
-# Run the tool
-# -------------------------------------
-
-Write-Host "Ejecutando wildintel-tools con uv..."
-Write-Host ""
-
-try {
-    uv run wildintel-tools --help
-} catch {
-    Write-Host "✕ Error ejecutando wildintel-tools." -ForegroundColor Red
-    exit 1
-}
-
-Write-Host ""
-Write-Host "✓ Instalación finalizada correctamente!" -ForegroundColor Green
+    Write-Hos
